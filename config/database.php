8dfb53213485fe5d23db50349754300e95ca455d
haqqer <ilhamserbaguna@gmail.com>
2018-05-02 07:05:02 +0700

    perubahan database

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'parkirscan.000webhostapp.com'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
